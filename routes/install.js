afd0eb7547376532e076ca980bba14e39ff469b9
Achala Dissanayake <AchalaD@99x.lk>
2017-11-13 20:38:24 +0530

    rote modularizing

--- routes/install.js
+++ routes/install.js
@@ -14,7 +14,30 @@ const apiKey = "7f3bc78eabe74bdca213aceb9cfcc1f4";
 const apiSecret = "d3141aefd842b5857b2048a3a229f4c8";
 
 router.get('/', (req, res) => {
-    res.send('Tracified - Shopify Modularized router');
+    const shop = req.query.shop;
+    if (shop) {
+
+        Shop.findOne({ 'name': shop }, 'name access_token', function (err, dbshop) {
+            if (err) return handleError(err);
+            if (dbshop && dbshop.access_token) {
+                res.status(200).send("Your shop has been authorized and token has been saved. Admin API can be accessed using the token ");
+            }
+            else {
+                const state = nonce();
+                const redirectUri = forwardingAddress + '/shopify/callback';
+                const installUrl = 'https://' + shop +
+                    '/admin/oauth/authorize?client_id=' + apiKey +
+                    '&scope=' + scopes +
+                    '&state=' + state +
+                    '&redirect_uri=' + redirectUri;
+
+                res.cookie('state', state);
+                res.redirect(installUrl);
+            }
+        });
+    } else {
+        return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+    }
 });
 
 module.exports = router;
