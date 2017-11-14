78d6e4cf9da87a00e0e7c1f0eb154205b4798257
Achala Dissanayake <AchalaD@99x.lk>
2017-11-14 17:42:17 +0530

    app unistall webhook registration and webhook handler

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router();const 
+Shop = require('../models/Shop');
 
 //index route
 router.get('/', (req, res) => {
