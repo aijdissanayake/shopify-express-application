88ec375cc0a2975727dcf9ae5808d9d6397e421e
Achala Dissanayake <AchalaD@99x.lk>
2017-11-16 21:34:44 +0530

    middleware modularization - webhook middleware

--- helpers/index.js
+++ helpers/index.js
@@ -22,7 +22,8 @@ module.exports = {
     verifyPayloadHMAC(data, hmac , apiSecret, next){
         data = JSON.stringify(data);
         var digest = crypto.createHmac('SHA256', apiSecret)
-        .update(new Buffer(data, 'utf8'))
+        .update(Buffer.from(data))
+        //.update(new Buffer(data, 'utf8'))
         //.update(data)
         .digest('base64');
         console.log("digest");
