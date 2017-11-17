60769ab403dc569e6796a6b37ac7509198945a24
Achala Dissanayake <AchalaD@99x.lk>
2017-11-17 16:09:53 +0530

    app unistall webhook registration and webhook handler

--- helpers/index.js
+++ helpers/index.js
@@ -25,7 +25,6 @@ module.exports = {
         //var digest = crypto.createHmac('SHA256','b90a3f90ec20de99390e7eeca171b4e9d4e9b3eae5c66b3b66862bbc444d78ff')
         .update(data)
         //.update(new Buffer(data, 'utf8'))
-        //.update(data)
         .digest('base64');
         console.log("digest");
         console.log(digest);
@@ -45,28 +44,6 @@ module.exports = {
         request(options).then(callback).catch(function (err) {
             return (err);
           });;
-    },
-
-    //test
-    verifyHmac(data, hmac) {
-        if (!hmac) {
-            console.log("no hmac");
-          return false;
-        } 
-        // else if (!data || typeof data !== 'object') {
-        //     console.log("no data aor incorrect type");
-        //     console.log(data); 
-        //     console.log(typeof data !== 'object');           
-        //   return false;
-        // }
-        const apiSecret = "d3141aefd842b5857b2048a3a229f4c8";
-        const sharedSecret = apiSecret;
-        const calculatedSignature = crypto.createHmac('sha256', sharedSecret).update(data).digest('hex');
-        console.log("hmac");
-        console.log(hmac);
-        console.log("calculatedSignature");
-        console.log(calculatedSignature);        
-        return calculatedSignature === hmac;
-      }
+    }
 
 }
--- middleware/index.js
+++ middleware/index.js
@@ -15,26 +15,5 @@ module.exports = {
         }
 
         return res.status(401).send("Unauthorized Webhook Request! body or HMAC header missing.");
-    },
-
-    verifyWebhook2(req, res, next) {
-        let hmac;
-        let data;
-        try {
-          hmac = req.get('X-Shopify-Hmac-SHA256');
-          data = req.rawBody;
-        } catch (e) {
-          console.log(`Webhook request failed from: ${req.get('X-Shopify-Shop-Domain')}`);
-          res.sendStatus(401);
-        }
-      
-        if (verifyHmac(JSON.stringify(data), hmac)) {
-          req.topic = req.get('X-Shopify-Topic');
-          req.shop = req.get('X-Shopify-Shop-Domain');
-          return next();
-        }
-      
-        return res.sendStatus(401);
-      }
-
+    }
 }
