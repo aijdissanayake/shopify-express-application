ed563cef62f27dec91d9074e1bb4a06ba21f36de
Achala Dissanayake <AchalaD@99x.lk>
2017-11-24 12:51:51 +0530

    sessions-cookie

--- index.js
+++ index.js
@@ -17,13 +17,18 @@ const apiKey = "7f3bc78eabe74bdca213aceb9cfcc1f4";
 const apiSecret = "d3141aefd842b5857b2048a3a229f4c8";
 const scopes = 'write_products,write_themes,write_orders';
 const forwardingAddress = "https://shopify-tracified.herokuapp.com";
-var bodyParser = require('body-parser');
-//react-view
+const bodyParser = require('body-parser');
+const session = require('client-sessions');
 const path = require('path');
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  cookieName: 'session',
+  secret: '7f3bc78eabe74bdca213aceb9cfcc1f4',
+  duration: 60 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
 
 app.set('port', process.env.PORT || 3000);
 
@@ -31,26 +36,28 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
-//Set up default mongoose connection
+
+//db connection
+  //Set up default mongoose connection
 var mongoDB = 'mongodb://shopify:Tracified@ds251435.mlab.com:51435/shopify-db';
 mongoose.connect(mongoDB, {
   useMongoClient: true
 });
-//Get the default connection
+  //Get the default connection
 var db = mongoose.connection;
-//Bind connection to error event (to get notification of connection errors)
+  //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+//routes
 app.use('/', index);
 app.use('/install', install);
 app.use('/webhook', webhook);
 app.use('/adminlink', adminlink);
-//test routes
+  //test routes
 app.use('/test', test);
-//react-view
-// Priority serve any static files.
+  //react-view
 app.use(express.static(path.resolve(__dirname, './react-app/build')));
-// All remaining requests return the React app, so it can handle routing.
+  // All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {
   response.sendFile(path.resolve(__dirname, './react-app/build', 'index.html'));
 });
