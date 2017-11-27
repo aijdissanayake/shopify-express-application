47973011bba91d69fc4d690dcac21b8bb6bdd4ee
Achala Dissanayake <AchalaD@99x.lk>
2017-11-27 17:51:29 +0530

    shop routes

--- routes/shop.js
+++ routes/shop.js
@@ -3,19 +3,19 @@ const router = express.Router();
 const Shop = require('../models/Shop');
 const shopAdminAPI = require('../helpers').shopAdminAPI;
 
-// router.all('/*', function (req, res, next) {
-//     if (req.session && req.session.shop) {
-//         console.log('cookie enbaled');
-//         req.shopRequestHeaders = {
-//             'X-Shopify-Access-Token': req.session.shop.access_token,
-//         };
-//         next();
-//     } else {
-//         console.log('cookies disabled');
-//         res.send('cookies disabled, You need to enable browser cookies to use the plugin without interruptions. Please enable cookies and retry.');
-//     }
+router.all('/*', function (req, res, next) {
+    if (req.session && req.session.shop) {
+        console.log('cookie enbaled');
+        req.shopRequestHeaders = {
+            'X-Shopify-Access-Token': req.session.shop.access_token,
+        };
+        next();
+    } else {
+        console.log('cookies disabled');
+        res.send('cookies disabled, You need to enable browser cookies to use the plugin without interruptions. Please enable cookies and retry.');
+    }
 
-// });
+});
 
 router.get('/products', (req, res) => {
     console.log('products');
@@ -37,7 +37,9 @@ router.get('/products', (req, res) => {
 });
 
 router.get('/orders', (req, res) => {
+    console.log('orders');
     shopAdminAPI('GET',  req.session.shop.name, '/admin/orders.json', req.shopRequestHeaders, null, function (orders) {
+        console.log('got orders');
         res.status(200).send(orders);
     });
 });
