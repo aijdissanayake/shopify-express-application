3bcb6bc936fa471f683ed166a5d557915d0a8d59
Achala Dissanayake <AchalaD@99x.lk>
2017-12-04 20:07:03 +0530

    removed accidentally set dist from tracking

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = express_1.Router();
-exports.router = router;
-router.get("/", (req, res) => { res.send("Tracified Ecoomerce Home page"); });
-router.get("/about", (req, res) => { res.send("Tracified Ecoomerce About page"); });
-router.get("/contact", (req, res) => { res.send("Tracified Contact Details"); });
-//# sourceMappingURL=index.js.map
