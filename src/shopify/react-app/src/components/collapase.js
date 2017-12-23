91cfc9fcb478d9417aa1f6a713df010e333a1024
Achala Dissanayake <aijdissanayake@gmail.com>
2017-12-23 08:46:31 +0530

    fulfill order functionality basics

--- src/shopify/react-app/src/components/collapase.js
+++ src/shopify/react-app/src/components/collapase.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Collapse, Row, Col} from 'reactstrap';
+import { Collapse, Row, Col } from 'reactstrap';
 import { Button, Card, ResourceList, Thumbnail } from '@shopify/polaris';
 import * as axios from 'axios';
 import { isUndefined } from 'util';
@@ -8,6 +8,7 @@ class CollapaseCard extends Component {
     constructor(props) {
         super(props);
         this.toggleCollapse = this.toggleCollapse.bind(this);
+        this.fulfillOrder = this.fulfillOrder.bind(this);
         this.state = { collapsed: true };
     }
 
@@ -15,8 +16,12 @@ class CollapaseCard extends Component {
         this.setState({ collapsed: !this.state.collapsed });
     }
 
+    fulfillOrder(){
+        alert("Order fulfilled")
+    }
+
     render() {
-        
+
         console.log("collapse products");
         console.log(this.props.products);
         console.log(typeof this.props.products);
@@ -27,11 +32,11 @@ class CollapaseCard extends Component {
                 const product = this.props.products.filter((product) => {
                     return product.id == resItem.product_id
                 });
-                
-                if (!isUndefined(product[0])){
+
+                if (!isUndefined(product[0])) {
                     productImage = product[0].images[0].src;
                     console.log(productImage);
-                }             
+                }
             }
 
             let resource = {
@@ -52,18 +57,20 @@ class CollapaseCard extends Component {
 
         return (
             <div>
-                <Row>
-                    <Col sm="6">
+                <Container>
                     <Row>
-                    <Col sm={{ size: 4, offset: 1 }}>
-                <Button primary onClick={this.toggleCollapse} >{this.state.collapsed ? "Show Items" : "Hide Items"}</Button>
-                    </Col>
-                    <Col sm={{ size: 4, offset: 1 }}>
-                <Button primary>Fulfill Order</Button>
-                    </Col>
+                        <Col sm="6">
+                            <Row>
+                                <Col sm="3">
+                                    <Button primary onClick={this.toggleCollapse} >{this.state.collapsed ? "Show Items" : "Hide Items"}</Button>
+                                </Col>
+                                <Col sm="3">
+                                    <Button primary onClick={this.fulfillOrder}>Fulfill Order</Button>
+                                </Col>
+                            </Row>
+                        </Col>
                     </Row>
-                    </Col>
-                </Row>
+                </Container>
                 <Collapse isOpen={!this.state.collapsed}>
                     <ResourceList
                         items={resourceList}
