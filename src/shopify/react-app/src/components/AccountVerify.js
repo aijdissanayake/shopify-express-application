766200545cdedc9834d26c2595d253bc286f71da
Achala Dissanayake <AchalaD@99x.lk>
2017-12-26 21:40:00 +0530

    account verification

--- src/shopify/react-app/src/components/AccountVerify.js
+++ src/shopify/react-app/src/components/AccountVerify.js
@@ -38,7 +38,7 @@ class AccountVerify extends Component {
         const tempToken = this.state.tempToken;
         axios.post('/shopify/tracified/account/verify', { tempToken })
           .then((result) => {
-            alert("Account verified successfully " + result.tracifiedToken);
+            alert("Account verified successfully " + JSON.parse(result).tracifiedToken);
             console.log(result);
           });
     }
