a6a32459c93b8a36233cfde567c42aaf4dbc6ec9
Achala Dissanayake <AchalaD@99x.lk>
2017-12-27 21:39:23 +0530

    installation view refined

--- src/shopify/react-app/src/components/Install.js
+++ src/shopify/react-app/src/components/Install.js
@@ -1,53 +1,49 @@
-import React, {Component} from 'react';
-import {Card,DisplayText,Button} from '@shopify/polaris';
+import React, { Component } from 'react';
+import { Card, DisplayText, Button } from '@shopify/polaris';
+import { Container, Row, Col } from 'reactstrap';
 import './install.css';
 
-class Installation extends Component{
-    render(){
-        var liStyle={
-            marginTop:'2%'
+class Installation extends Component {
+    render() {
+        var liStyle = {
+            marginTop: '2%'
         }
 
-        var lastIntPara={
-            marginBottom:'2%'
+        var lastIntPara = {
+            marginBottom: '2%'
         }
 
-        return(            
+        return (
             <div>
                 <div class="InstallTitle">
-                    <DisplayText size="medium">Installation Instructions</DisplayText>                    
+                    <DisplayText size="medium">Installation Instructions</DisplayText>
                 </div>
                 <div class="InstallVideo">
-                    <iframe width="560" height="415" src="https://www.youtube.com/embed/6MaOPdQPvow" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+                    <iframe width="560" height="415" src="https://www.youtube.com/embed/1zVRoaq-wDA?rel=0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
                 </div>
                 <div class="InstallDescription">
                     <Card>
                         <ol>
                             <li style={liStyle}>
-                                From the Shopify Admin panel, click on <a href="#">"Online Store"</a> 
+                                From the Shopify Admin panel, click on <code>"Online Store"</code>
                             </li>
                             <li>
-                                Click on the <code>Actions</code> dropdown button for the current published theme and select <code>Edit HTML/CSS</code>.                        
+                                Click on the <code>Actions</code> dropdown button for the current published theme and select <code>Edit Code</code>.
+                            </li>
+                            <li>Select the file to be edited depending on the theme.
+                                <ul>
+                                    <br />
+                                    <li>For a Sectioned Theme, locate and click on product.liquid under the Templates folder to open it for editing. </li>
+                                    <li>For a Non-Sectioned Theme, locate and click on product-template.liquid under the Sections folder to open it for editing. <br />(The liquid file should contain the details of the products.)</li>
+                                </ul>
+
                             </li>
                             <li>
-                                Under the <code>Templates</code>  folder, locate and click on <code>cart.liquid</code> to open it for editing.
-                                (for sectioned themes, look in the <code>Sections</code> folder, for <code>cart-template.liquid</code> instead) 
-                            </li>
-                            <li>
-                                Locate the closing tag of the cart form 
+                                Subsequently, following liquid code snippet should be included in the selected liquid file and save the changes. <br /><br />
                                 <code>
-                                    {/* </form>     */}
+                                    {"{% include 'tracified' %}"}
                                 </code>
-                            </li>
-                            <li>
-                                Insert the following liquid code snippet
-                                <code> 
-                                    {/* {% include 'deliverydate' %}  */}
-                                </code>
-                                just before the cart form closing tag 
-                            </li>
-                            <li>
-                                Save your changes
+
                             </li>
                         </ol>
 
@@ -57,18 +53,11 @@ class Installation extends Component{
 
                         <div class="lastInst">
                             <p>
-                                To uninstall, simply remove the code snippet inserted into the theme.
-                            </p>
-                            <p style={lastIntPara}>
-                                NOTE: Leaving the snippet in place after uninstallation, will have no adverse effect on the site
+                                To uninstall, simply remove the code snippet inserted into the theme. Removing or leaving the assets which were uploaded during installation will make no adverse effect on the site.
                             </p>
                         </div>
-
-                        {/* <button class="AssistanceBtn" size="lg">
-                            Request Installation Assistance
-                        </button>{' '} */}
-                        <Button class="AssistanceBtn" primary>Request Installation Assistance</Button>
                     </Card>
+                    <br />
                 </div>
             </div>
         );
