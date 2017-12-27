2e05bad63bf826fbed8484569de90f9c2c3cc509
Achala Dissanayake <AchalaD@99x.lk>
2017-12-27 20:02:57 +0530

    account verification

--- src/shopify/react-app/src/components/AccountVerify.js
+++ src/shopify/react-app/src/components/AccountVerify.js
@@ -39,8 +39,11 @@ class AccountVerify extends Component {
         axios.post('/shopify/tracified/account/verify', { tempToken })
             .then((response) => {
                 // alert("Account verified successfully " + result.data);
-                window.location.href = response.redirect;
+                window.location.replace('/shopify/main-view');
+                // window.location.href = response.redirect;
                 console.log(response);
+            }).catch((err) =>{
+                console.log(err);
             });
     }
 
@@ -52,14 +55,14 @@ class AccountVerify extends Component {
                         <Card.Section>
                             <p> Looks like you haven't connected a Tracified Account yet.</p>
                             <p> Please Contact your Tracified Admin and submit the temporary token here to connect an account for further proceedings</p>
-                        </Card.Section>
-                        <Card.Section>
+                        <Row>
                             <Col sm="8" offset="2">
                                 <TextField onChange={this.onChange} value={this.state.tempToken} label="Enter the access token" />
                             </Col>
                             <Col sm="4" offset="2">
                                 <Button primary onClick={this.onClick}>Connect</Button>
                             </Col>
+                        </Row>                            
                         </Card.Section>
                         <Card.Section></Card.Section>
                     </FormLayout>
