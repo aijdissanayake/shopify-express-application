25c70f6098a8c2c5c5aa5e57515788f1554be8f4
dammakaru <dammakaru@gmail.com>
2018-01-08 21:28:04 +0530

    updated cards in unfullfilled orders view to collapse

---
+++ src/shopify/react-app/src/components/CollapseMain.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import CollapaseCards from './collapase';
+import * as axios from 'axios';
+import { Row, Col, Collapse } from 'reactstrap';
+import { Thumbnail, Card, Page, Button, Stack, TextStyle } from '@shopify/polaris';
+const QRCode = require('qrcode.react');
+
+class CollapseMain extends Component {
+
+    constructor() {
+        super();
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            collapase: false
+        };
+    }
+
+    toggle() {
+        this.setState({ collapse: !this.state.collapse });  
+    }
+
+    render() {
+
+        let buttonProps = this.state.collapse ? {disclosure:false, text:"Hide Details"} : {disclosure:true, text:"View Details"}
+    
+        return (
+  
+            <Card key={this.props.order.order_number} sectioned subdued={false}>    
+                <Stack>
+                    <Stack.Item >
+                        <TextStyle variation="strong">{this.props.title}</TextStyle>
+                    </Stack.Item>
+                    <Stack.Item>
+                        <TextStyle variation="subdued"><strong>Created on:</strong> {this.props.order.created_at}</TextStyle>
+                    </Stack.Item>
+                    <Stack.Item fill>
+                        <TextStyle variation="subdued"><strong>Customer:</strong> {this.props.order.customer}</TextStyle>
+                    </Stack.Item>
+                    <Stack.Item>
+                        <Button 
+                            size="slim" 
+                            outline 
+                            disclosure={buttonProps.disclosure}  
+                            onClick={this.toggle} 
+                            style={{ marginBottom: '1rem' }}
+                        >
+                            {buttonProps.text}
+                        </Button>
+                    </Stack.Item>
+                </Stack>
+                
+                <Collapse isOpen={this.state.collapse}>
+                    <Row style={{paddingTop: '1rem' }}>
+                        <Col sm="10">
+                            <CollapaseCards itemArray={this.props.order.lineItems} products={this.props.productsProp} orderID={this.props.order.id
+                            } />
+                        </Col>
+                        <Col sm="2">
+                            <QRCode value={this.props.qrVal} />
+                        </Col>
+                    </Row>
+                </Collapse>   
+
+            </Card>                
+        );
+    }
+}
+
+export default CollapseMain;
