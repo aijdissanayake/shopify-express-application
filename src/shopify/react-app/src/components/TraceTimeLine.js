bcf08b40a0b81e642f319d8f6950a5755e016ed6
Achala Dissanayake <aijdissanayake@gmail.com>
2018-01-12 01:38:41 +0530

    new time line view

--- src/shopify/react-app/src/components/TraceTimeLine.js
+++ src/shopify/react-app/src/components/TraceTimeLine.js
@@ -3,15 +3,11 @@ import Loading from './Loading';
 import * as axios from 'axios';
 import {
     Spinner,
-    DisplayText,
-    TextStyle,
     Card,
-    Page,
-    Avatar,
-    Heading
+    Page
 } from '@shopify/polaris';
 import { Row, Col, Container } from 'reactstrap';
-
+import { Timeline, TimelineEvent } from 'react-event-timeline';
 
 class TraceTimeLine extends Component {
 
@@ -25,10 +21,9 @@ class TraceTimeLine extends Component {
 
     componentDidMount() {
         const traceURL = "/shopify/tracified/trace/" + this.props.match.params.orderID + "/" + this.props.match.params.itemID;
-
         axios({
             method: 'get',
-            url: traceURL, 
+            url: traceURL,
             headers: {
                 'Content-Type': 'text/plain;charset=utf-8',
             },
@@ -45,24 +40,6 @@ class TraceTimeLine extends Component {
 
     render() {
 
-
-        var description = {
-            padding: "2% 2% 2% 7%",
-            fontSize: 16
-        };
-
-        var line = {
-            "margin": "90% 35% 35% 35%",
-            "border-size": "0.5px",
-            "border-width": "thin",
-            "border-style": "solid",
-            "border-color": "black",
-            "height": "500px",
-            "width": "0%"
-        }
-
-
-
         if (this.state.istimelineLoading) {
             return <Loading />;
         }
@@ -70,61 +47,43 @@ class TraceTimeLine extends Component {
             console.log(this.props.match.params.orderID);
             console.log(this.props.match.params.itemID);
             return (
-                <Page title="Trace Back Timeline" separator>
-                    <DisplayText size="small">
+                <div style={{ backgroundColor: '#f4f6f8' }}>
+                    <Page title="Trace Back Timeline" separator>
+                        <Timeline>
+                            {this.state.timeline.items.map((stage, index) => {
 
-                        {this.state.timeline.items.map((stage, index) => {
-                            return (
-                                <Card key={stage.stage}
-                                >
-                                    <Card.Section>
-                                        <Row>
-                                            <Col sm='1'>
-                                                <Avatar
-                                                    customer
-                                                    name="Farrah"
-                                                    source={stage.icon}
-                                                />
-                                            </Col>
-                                            <Col sm='11'>
-                                            <TextStyle variation='strong' >
-                                                {index+1}.&nbsp;{stage.title}
+                                let titleText = (index + 1) + ". " + stage.title;
+                                let descriptionText = stage.description;
 
-                                            </TextStyle>
-                                            </Col>   
-                                        </Row>
-                                        <div >
-                                            <Row>
-                                                <Col sm="1">
-                                                    {/* <div style={line}></div> */}
-                                                    <hr width="1" size="500"/>
-                                                </Col>
-                                                <Col sm="11">
-                                                    <Heading> <div > {stage.description} </div> </Heading> <br />
+                                var ico = (<svg height="20" width="20" >
+                                    <image width="20" height="20" xlinkHref={stage.icon} />
+                                </svg>);
 
-                                                    {
+                                return (
+                                    <TimelineEvent
+                                        key={index}
+                                        title={titleText}
+                                        titleStyle={{ fontSize: 17 }}
+                                        subtitle={descriptionText}
+                                        subtitleStyle={{ fontSize: 15 }}
+                                        icon={ico}
+                                        iconColor="#6fba1c"
+                                        contentStyle={{ fontSize: 13 }}
+                                    >
+                                        {
 
-                                                        Object.keys(stage.data).map(function (key) {
-                                                            return <div> {stage.data[key].title}</div>;
-                                                        })
+                                            Object.keys(stage.data).map(function (key) {
+                                                return <div key={key}> {stage.data[key].title}</div>;
+                                            })
 
-                                                    }
-                                                </Col>
-                                            </Row>
-                                        </div>
+                                        }
+                                    </TimelineEvent>
+                                );
 
-
-                                    </Card.Section>
-                                </Card>
-
-
-
-                            )
-                        })}
-
-                    </DisplayText>
-                </Page>
-
+                            })}
+                        </Timeline>
+                    </Page>
+                </div>
             );
         }
     }
